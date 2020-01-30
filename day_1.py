589760fa0df4c9b65a01c6d4d01d5a9813bda558
Alex Bainton <alex.bainton@seeingmachines.com>
2020-01-31 10:08:00 +1100

    Add thorough typing to day_1; Convert map call result to list.

--- day_1.py
+++ day_1.py
@@ -5,17 +5,17 @@ from pathlib import Path
 from typing import List
 
 def required_fuel(mass: float) -> float:
-    required_fuel = math.floor(mass / 3) - 2
+    required_fuel: float = math.floor(mass / 3) - 2
     return required_fuel
 
 def read_spacecraft_module_weights(filename) -> List[float]:
     weights_str: List[float] =  list(Path(filename).open('r'))
-    weights_float: List[float] = map(float, weights_str)
+    weights_float: List[float] = list(map(float, weights_str))
     return weights_float
 
 def main(args):
-    module_weights = read_spacecraft_module_weights(args[1])
-    total_fuel = 0
+    module_weights: List[float] = read_spacecraft_module_weights(args[1])
+    total_fuel: float = 0
     for module_weight in module_weights:
         total_fuel += required_fuel(module_weight)
     print(total_fuel)
